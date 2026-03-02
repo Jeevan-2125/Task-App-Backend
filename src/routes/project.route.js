cff2e4c4c70b685a729b6786b54952cc83a3ca54
Jeevan-2125 <jeevangowdasjeevan@gmail.com>
2026-03-02 12:52:39 +0530

    Update project.route.js

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { getMyProjects, createProject } from '../controllers/project.controller.js';
 import { verifyToken, isAdmin } from '../middlewares/auth.middleware.js';
 import { updateProjectStatus } from '../controllers/project.controller.js';
+import upload from '../middlewares/upload.js';
 
 const router = express.Router();
 
 router.get('/my-projects', verifyToken, getMyProjects);
 router.post('/create', verifyToken, isAdmin, createProject);
-// router.post('/update-status', verifyToken, updateProjectStatus);
 router.post('/update-status', verifyToken, updateProjectStatus);
+router.post('/projects/create', verifyToken, upload.single('file'), createProject);
 
 export default router;
